91e1f0f322a712e7b1453a135b4ef7ed6e3ce4ff
ian otieno <ianotieno23@gmail.com>
2023-09-06 09:49:44 +0300

    update

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import func
 from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
